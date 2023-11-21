11e3e52174b07e95834d5f285ae37d8836c861ee
Ben Anandappa <ben.anandappa@gmail.com>
2023-11-21 12:38:08 -0600

    handle token signature failure

--- assets/js/fetch_error_handling.js
+++ assets/js/fetch_error_handling.js
@@ -1,9 +1,13 @@
-const JWT_MISSING = "Missing cookie \"access_token_cookie\""
-const JWT_EXPIRED = "Token has expired"
+const JWT_MISSING = "Missing cookie \"access_token_cookie\"";
+const JWT_EXPIRED = "Token has expired";
+const JWT_SIG_FAILURE = "Signature verification failed";
 
 async function handleGraphQLError(error, loginUrl, credential_data, refreshUrl, graphQLUrl, query) {
     if (isUnauthorizedError(error)) {
         await handleUnauthorizedError(error, loginUrl, credential_data, refreshUrl);
+        return await getGraphQLData(graphQLUrl, query);
+    } else if (isSigFailure(error)) {
+        await handleSigFailure(error, loginUrl, credential_data)
         return await getGraphQLData(graphQLUrl, query);
     } else {
         console.error('Error fetching protected resource:', error);
@@ -14,6 +18,9 @@ async function handleGraphQLError(error, loginUrl, credential_data, refreshUrl, 
 async function handleRestError(error, loginUrl, credential_data, refreshUrl, restUrl) {
     if (isUnauthorizedError(error)) {
         await handleUnauthorizedError(error, loginUrl, credential_data, refreshUrl);
+        return await getRestData(restUrl);
+    } else if (isSigFailure(error)) {
+        await handleSigFailure(error, loginUrl, credential_data)
         return await getRestData(restUrl);
     } else {
         console.error('Error fetching protected resource:', error);
@@ -44,6 +51,23 @@ async function handleRefresh(refreshUrl, loginUrl, credential_data) {
     }
 }
 
+async function handleSigFailure(error, loginUrl, credential_data) {
+    // NOTE
+    // Ideally log out user if sig failure
+    // Similar to above, memberspace doesn't have an API to log out user
+    // So for user experience, we will re-authenticate the user for now
+    const errorBody = await error.json();
+    if (errorBody.msg === JWT_SIG_FAILURE) {
+        await login(loginUrl, credential_data);
+    } else {
+        throw error; 
+    }
+}
+
 function isUnauthorizedError(error) {
     return error instanceof Response && error.status === 401;
+}
+
+function isSigFailure(error) {
+    return error instanceof Response && error.status === 422;
 }
