a1b0d3d19dffc7faac24a7c05b1ee8accb708dc5
Ben Anandappa <ben.anandappa@gmail.com>
2023-11-20 22:13:14 -0600

    Add wait for MemberSpace to become ready to get credentials

--- assets/js/set_tree.js
+++ assets/js/set_tree.js
@@ -1,33 +1,64 @@
-var getMsReadyPromise = () =>
-  new Promise(resolve => {
-    if (MemberSpace.ready) {
-      // Ready event is already fired, so let's not wait for it, it will not be fired again
-      resolve(window.MemberSpace.getMemberInfo());
-    } else {
-      // MS widget is not yet ready, let's subscribe for the event
-      const handleReady = ({ detail }) => {
-        resolve(detail);
-        // Unsubscribe ourselves, this allows GC to collect all related memory
-        document.removeEventListener('MemberSpace.ready', handleReady);
-      };
+// Function to wait until MemberSpace is defined
+function waitForMemberSpace() {
+  return new Promise((resolve, reject) => {
+    const maxWaitTime = 10000; // Maximum wait time in milliseconds
+    const intervalTime = 100;  // Interval time to check in milliseconds
+    let elapsedTime = 0;
 
-      // Listen to ready event
-      document.addEventListener('MemberSpace.ready', handleReady);
-    }
+    const interval = setInterval(() => {
+      if (typeof MemberSpace !== 'undefined') {
+        clearInterval(interval);
+        resolve();
+      } else {
+        elapsedTime += intervalTime;
+        if (elapsedTime >= maxWaitTime) {
+          clearInterval(interval);
+          reject('MemberSpace did not become available within the maximum wait time.');
+        }
+      }
+    }, intervalTime);
   });
+}
+
+// Function to get MemberSpace information
+var getMsReadyPromise = () => new Promise((resolve, reject) => {
+  // Check if MemberSpace is ready
+  if (MemberSpace.ready) {
+    try {
+      resolve(window.MemberSpace.getMemberInfo());
+    } catch (error) {
+      reject('Error fetching MemberSpace member info: ' + error);
+    }
+  } else {
+    const handleReady = ({ detail }) => {
+      try {
+        resolve(detail);
+      } catch (error) {
+        reject('Error in MemberSpace.ready event: ' + error);
+      }
+      document.removeEventListener('MemberSpace.ready', handleReady);
+    };
+
+    document.addEventListener('MemberSpace.ready', handleReady);
+  }
+});
 
 async function setTreeData(loginUrl, refreshUrl, restUrlBase, tree_id) {
     var restUrl = restUrlBase + tree_id;
     var isLoggedIn = true;
     // Define the credentials
     var credential_data = {
-        email: 'email',
-        user_id: 'user_id'
+        email: '',
+        user_id: ''
     };
 
     try {
-        const { data } = await getMsReadyPromise();
-        isLoggedIn = data.isLoggedIn;
+        // Wait for MemberSpace to become available
+        await waitForMemberSpace();
+        console.log('MemberSpace is now available.');
+
+        const data = await getMsReadyPromise();
+        isLoggedIn = data && data.isLoggedIn;
         if (isLoggedIn) {
             credential_data.email = data.memberInfo.email;
             credential_data.user_id = data.memberInfo.id;
